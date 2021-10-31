c70a48ef46bc555c414b5774a26b88f568f014ae
MontseOrtiz <montse_2908@hotmail.com>
2021-10-31 17:09:40 -0600

    agregando jumbo y modificando configuracion del proyecto

---
+++ src/components/Jumbo.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import {StyledJumbo} from '../styles/components'
+
+export default function Jumbo() {
+    return (
+        <StyledJumbo>
+            <div>
+                <h2>
+                    ¡Consigue las mejores playeras para developers !
+                </h2>
+                <small>Playeras con mensajes cool para todos los developer que aman codear con estilo</small>
+            </div>
+            
+        </StyledJumbo>
+
+    )
+}
